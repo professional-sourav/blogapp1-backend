1d277b3f56a4321f12e04e5468fd419f66bd53b8
Sourav <shawn@atarim.io>
2024-06-25 19:33:22 +0530

    Added upodate and delete API

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import { createNewPost, getAllPosts } from "../models/post"
+import { createNewPost, deleteSinglePost, getAllPosts, getPostById, updateSinglePost } from "../models/post"
 import { Post } from "../../types/postType"
 
 export const all = async (req: Request, res: Response) => {
@@ -37,10 +37,48 @@ export const single = async (id: number) => {
 
 }
 
-export const update = async (id: number) => {
+export const update = async (req: Request, res: Response) => {
 
+    const post = req.body as Post;
+
+    const existingPost = await getPostById(req.params.id)
+    .catch((err) => {
+        res.status(500).json({ message: err.message })
+    })
+
+    // if (!existingPost) {
+    //     res.status(404).json({ message: "Post not found" })
+    // }
+
+    if (existingPost) {
+        const updatedPost = await updateSinglePost(req.params.id, post).catch((err) => {
+            console.log(err.message);
+            res.status(500).json({ message: err.message })
+        })
+
+        if (updatedPost) {
+            res.status(200).json(updatedPost)    
+        }
+    }
 }
 
-export const remove = async (id: number) => {
+export const remove = async (req: Request, res: Response) => {
 
+    const post = req.body as Post;
+
+    const existingPost = await getPostById(req.params.id)
+    .catch((err) => {
+        res.status(500).json({ message: err.message })
+    })
+
+    if (existingPost) {
+
+        deleteSinglePost(req.params.id)
+        .then((deletedPost) => {
+            res.status(200).json(deletedPost)
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err.message })
+        })
+    }
 }
--- src/models/post.ts
+++ src/models/post.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { Post } from "../../types/postType";
 import { Post as PostModel } from "../config/schemas/postSchema"
 
@@ -19,4 +20,39 @@ export const createNewPost = async (post: Post) => {
     await newPost.save()
 
     return newPost;
+}
+
+export const getPostById = async (id: string) => {
+
+    let postId = null;
+
+    try {
+        postId = new mongoose.Types.ObjectId(id);
+    } catch (error: any) {
+        throw new Error("Invalid post ID")
+    }    
+
+    const post = await PostModel.findById(postId)
+
+    return post;    
+}
+
+export const updateSinglePost = async (id: string, post: Post) => {
+
+    const updatedPost = await PostModel.findOneAndUpdate({
+        _id: id
+    }, post, {
+        new: true
+    })
+
+    return updatedPost;
+}
+
+export const deleteSinglePost = async (id: string) => {
+
+    const deletedPost = await PostModel.findOneAndDelete({
+        _id: id
+    })
+
+    return deletedPost;
 }
